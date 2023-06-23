8017c315c403a370fda308b0d840deea38db9f13
Harshit Singh tomar <119979451+HarshitSinghT0mar@users.noreply.github.com>
2023-06-24 00:47:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CreateArea from './components/CreateArea'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+     <Header />
+     <CreateArea />
+    </div>
+  )
+}
+
+export default App
